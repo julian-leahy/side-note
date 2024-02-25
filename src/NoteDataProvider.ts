ea5e7a385bf4afe36645e2133c35fd9afef92c47
Julian Leahy <84889719+leahyjulian@users.noreply.github.com>
2024-02-25 16:06:21 +1300

    Add icons in view to create, delete and refresh

--- src/NoteDataProvider.ts
+++ src/NoteDataProvider.ts
@@ -3,6 +3,31 @@ import * as fs from "fs";
 import * as path from "path";
 
 class NoteDataProvider implements vscode.TreeDataProvider<vscode.TreeItem> {
+  // listen for changes in the tree view
+  private _onDidChangeTreeData: vscode.EventEmitter<
+    vscode.TreeItem | undefined | null | void
+  > = new vscode.EventEmitter<vscode.TreeItem | undefined | null | void>();
+
+  readonly onDidChangeTreeData: vscode.Event<
+    vscode.TreeItem | undefined | null | void
+  > = this._onDidChangeTreeData.event;
+
+  // path to local dir where note are located
+  private noteDir: string;
+
+  constructor() {
+    this.noteDir = vscode.workspace
+      .getConfiguration()
+      .get("storage.path") as string;
+  }
+
+  // call this anytime to refresh tree view
+  refresh(): void {
+    this._onDidChangeTreeData.fire();
+  }
+
+  // this needs to be implemented (interface) but not required for extension
+  // so just return what it is given
   getTreeItem(
     element: vscode.TreeItem
   ): vscode.TreeItem | Thenable<vscode.TreeItem> {
@@ -12,7 +37,37 @@ class NoteDataProvider implements vscode.TreeDataProvider<vscode.TreeItem> {
   getChildren(
     element: vscode.TreeItem
   ): vscode.ProviderResult<vscode.TreeItem[]> {
-    return;
+    // check the dir to where the notes will be stored exists!
+    if (!fs.existsSync(this.noteDir)) {
+      vscode.window.showInformationMessage("Note directory does not exist.");
+      return Promise.resolve([]);
+    }
+
+    // array of all files in this directory
+    const files = fs.readdirSync(this.noteDir);
+    return files
+      .filter((file) => {
+        const ext = path.extname(file).toLowerCase();
+        return (
+          !file.startsWith(".") &&
+          ![".png", ".jpeg", ".webp", ".jpg"].includes(ext)
+        );
+      })
+      .sort((a, b) => {
+        const extA = path.extname(a);
+        const extB = path.extname(b);
+        return extA.localeCompare(extB);
+      })
+      .map((file) => {
+        const treeItem = new vscode.TreeItem(path.basename(file));
+        treeItem.resourceUri = vscode.Uri.file(path.join(this.noteDir, file));
+        treeItem.command = {
+          command: "extension.openFile",
+          title: "Open File",
+          arguments: [treeItem.resourceUri],
+        };
+        return treeItem;
+      });
   }
 }
 
--- src/extension.ts
+++ src/extension.ts
@@ -10,6 +10,11 @@ export function activate() {
   vscode.window.createTreeView("SideNoteView", {
     treeDataProvider: noteDataProvider,
   });
+
+  vscode.commands.registerCommand("extension.openFile", async (uri) => {
+    const document = await vscode.workspace.openTextDocument(uri);
+    await vscode.window.showTextDocument(document);
+  });
 }
 
 export function deactivate() {}
